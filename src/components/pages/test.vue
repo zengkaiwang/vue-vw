11f6701bb1ffbf1c3e688bfde9e4514a95bad37b
wangzengkai <wangzengkai@zuoyebang.com>
2019-04-04 14:27:55 +0800

    实现动态加载弹框组件

--- src/components/pages/test.vue
+++ src/components/pages/test.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
     <!-- 弹框提示 -->
-    <toast></toast>
-    <!-- loading -->
-    <loading :data="loadParams"></loading>
+    <!-- <toast></toast> -->
+
+    <!-- toast弹框方案二 -->
+    <input type="button" value="显示弹窗" @click="showToast">
+
+    <!-- loading组件 -->
+    <!-- <loading :data="loadParams"></loading> -->
   </div>
 </template>
 
 <script>
-import toast from '../common/toast/toast';
-import loading from '../common/loading/loading';
+// import toast from '../common/toast/toast';
+// import loading from '../common/loading/loading';
+// import toastRegistry from '@/components/common/toast/index'
+
 export default {
-  components: {toast, loading},
+  // components: {toast, loading},
   data() {
     return {
       loadParams: {
         bottom: 30
       }
     }
+  },
+  methods: {
+    showToast () {
+      this.$toast('我是弹出消息', 1000)
+    }
   }
+
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import '@/style/global.less'
 
+import toastRegistry from '@/components/common/toast/index'
+Vue.use(toastRegistry);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
